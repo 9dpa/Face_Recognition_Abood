f746b898b37d4d06ccb5e29847c528db64b0a822
9dpa <102618776+9dpa@users.noreply.github.com>
2022-03-29 21:26:52 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,63 @@
+"Welcome to AbdurHman adel Program"
+"For our study in multimedia, we learned a lot from offices and how to deal
+" with them. My project now is a program that identifies people and
+" knows them through a computer camera."
+"If we store some pictures of the people we want to know in a 
+"special folder and name these pictures with the names of the
+" people as a kind of storage for their names.
+"His office has used face recognition
+"What distinguishes this library from others is that the ability to distinguish in it reaches 99%
+"
+"
+
+
+"AND I user 3 pyhton files :
+"First : Image Compression -> This file compares one image with another so that it can 
+"bring the features of the face, differentiate and identify the person.
+
+"Secound : Main_Vedio -> This file performs the conditional Encode check of the function
+" in order to fetch the class file and the comparison file, and then it fulfills the condition.
+
+"Theard : Simple Faces - > they do 
+"1- Resize frame for a faster speed.
+"2-then Load Images
+"3-Store image encoding and names
+"4-Get the filename only from the initial file path.
+"5-Store file name and file encoding
+"6-Find all the faces and face encodings in the current frame of video
+"Convert the image from BGR color (which OpenCV uses) to RGB color 
+"(which face_recognition uses
+"7-See if the face is a match for the known face(s)
+"8-Convert to numpy array to adjust coordinates with frame resizing quickly
+"
+
+
+
+                       #------ IF YOU WANT TO CONTACT ME ------#
+My_NAME : "Abdurlhman Alhazmi "
+
+PHONE : "711681371"
+
+Email = "King565718@gmail.com "
+
+
+
+
+
+
+
+
+                         " i Used Labires : 
+                     "1- import face_recognition
+                               "2 -cv2
+                               " 3- os
+                                "4- glob
+                                "5- numpy 
+
+
+
+
+
+
+
+
